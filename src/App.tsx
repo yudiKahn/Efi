f18182972f235376f66104be5e41b0681f18608d
Yehuda Kahn <109089553+YehudaKahn@users.noreply.github.com>
2026-07-14 21:17:30 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-extrabold tracking-tight text-sky-400">
+        Vite + React + Tailwind v4 🚀
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
